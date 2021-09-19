60bace63bbfbbe1845365b33b96ce5c8dabb9091
Pierre Dahmani <git@pierrefhapp.dev>
2021-09-19 02:41:40 +0200

    lsp still not working :[

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,10 @@ module Main where
 import qualified FirstSteps (add, evens, fac, testPrint)
 import qualified MyLib (someFunc)
 
--- determines that main will do IO stuff?
+-- IO determines that main will do impure stuff?
 main :: IO ()
 main = do
+  putStrLn "Hello, world!"
   -- prints hello world to stdout
   FirstSteps.testPrint
   -- prints result of factorial function for n = 3 to stdout
@@ -17,7 +18,7 @@ main = do
   -- let declares variable in local scope. looks like it is acessable in whole
   -- do block
   -- in haskell it seems like functions are curried by default
-  let plusTwo = FirstSteps.add (2)
+  let plusTwo = FirstSteps.add 2
   print (plusTwo 3)
   print (plusTwo 4)
 
