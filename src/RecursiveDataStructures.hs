485d3adae4455ba3fa38783e68b487143368327c
git@pierrefhapp.dev <git@pierrefhapp.dev>
2021-10-15 12:13:23 +0200

    1h. code binary search tree in haskell. took an hour and I still get the warning for non exhaustive pattern matches. Maybe look at solution and don't waste more time. Or try to get it yourself

--- src/RecursiveDataStructures.hs
+++ src/RecursiveDataStructures.hs
@@ -3,25 +3,31 @@
 -- data list' a :: a list'
 -- either empty list or some value with a list of values of same type.
 data List' a = TestEmpty | Cons a (List' a) deriving (Show, Eq, Ord, Read)
+
 -- cons is ':' in normal lists.
 -- calling cons in infix to create a list'
 testList' = 3 `Cons` TestEmpty
+
 testLargeList' = 3 `Cons` (4 `Cons` (5 `Cons` TestEmpty))
 
 -- "We can define functions to be automatically infix by making them comprised
 -- of only special characters. We can also do the same with constructors, since
 -- they're just functions that return a data type. So check this out."
 infixr 5 :.:
+
 data List a = Empty | a :.: (List a) deriving (Show, Read, Eq, Ord)
+
 testLista = 2 :.: 3 :.: 4 :.: Empty
+
 -- infixr val symbol -> means that symbol is right associative with value val.
 -- infixl val symbol -> means that symbol is left associative with value val.
 -- example: + is defined with fixity 6, infixr6.
+
 -- * is defined with fixity 7. inifxr7.
+
 -- Meaning that both are right associative -> a * b * c === a * (b * c)
 -- Since * has higher right fixity -> a + b * c === a + (b * c)
 -- BUT a * b + c !== a * (b + c)
-
 
 -- concat two lists is defined as:
 -- infixr 5  ++
@@ -30,10 +36,12 @@ testLista = 2 :.: 3 :.: 4 :.: Empty
 -- (x:xs) ++ ys = x : (xs ++ ys)
 -- custom concat two lists. uses our previous definitions :] (Empty, List, :.:,
 -- etc.)
-infixr 5  .++
+infixr 5 .++
+
 (.++) :: List a -> List a -> List a
 Empty .++ ys = ys
 (x :.: xs) .++ ys = x :.: (xs .++ ys)
+
 -- TODO(pierre): understand this text..
 -- NOTE: xs is List a x is x and :.: is :.. -> (x :.: xs) === a :.. (List a)
 -- "Notice how we pattern matched on (x :-: xs). That works because pattern
@@ -44,13 +52,46 @@ Empty .++ ys = ys
 -- that, normal prefix constructors or stuff like 8 or 'a', which are basically
 -- constructors for the numeric and character types, respectively."
 
-
--- BINARY SEARCH TREE (BST)
+-- BINARY SEARCH TREE (BinaryTree)
 -- LEFT FROM = -> type constructor -> cant be created as value
 -- RIGHT FROM = -> value constructor -> can be created as value
--- -> There will never be BST 1 as value. Since BST 1 is either EmptyTree or
+-- -> There will never be BinaryTree 1 as value. Since BinaryTree 1 is either EmptyTree or
 -- Node 1 (EmptyTree | Node val (EmptyTree | Node val ....))
-data BST a = EmptyTree | Node a (BST a) (BST a) deriving (Show, Read, Eq)
-emptyTree a = Node a EmptyTree EmptyTree
+data BinaryTree a
+  = EmptyTree
+  | Node a (BinaryTree a) (BinaryTree a)
+  deriving (Show, Read, Eq)
 
--- TODO: continue at binary search tree.
+-- helper function that determines whether the tree is empty.
+isEmptyTree :: BinaryTree a -> Bool
+isEmptyTree EmptyTree = True
+isEmptyTree _ = False
+
+-- helper function that creates a base tree(node empty empty) for given number.
+createBaseTree :: a -> BinaryTree a
+createBaseTree x = Node x EmptyTree EmptyTree
+
+-- three cases: eq -> drop, bigger -> right, smaller -> left
+-- Notation: first BinaryTree(a) is the left leaf and second BinaryTree(a) is the right leaf.
+insertInBinaryTree :: (Ord a) => BinaryTree a -> a -> BinaryTree a
+insertInBinaryTree EmptyTree input = Node input EmptyTree EmptyTree
+insertInBinaryTree (Node x leftTree rightTree) input
+  | input == x = Node x leftTree rightTree
+  | input < x =
+    if isEmptyTree leftTree
+      then Node x (createBaseTree input) rightTree
+      else Node x (insertInBinaryTree leftTree input) rightTree
+  | input > x =
+    if isEmptyTree rightTree
+      then Node x leftTree (createBaseTree input)
+      else Node x leftTree (insertInBinaryTree rightTree input)
+
+-- generate some numbers to insert into tree
+genNumbers = map (\x -> if even x then x + 10 else x) [1 .. 5]
+
+-- insert all these numbers into BinaryTree with base 10
+-- insertNumbersIntoTree =
+--   foldl (\acc x -> insertInBinaryTree acc x) (createBaseTree 10) genNumbers
+
+insertNumbersIntoTree =
+  foldl (\acc x -> insertInBinaryTree acc x) (createBaseTree 10) genNumbers
