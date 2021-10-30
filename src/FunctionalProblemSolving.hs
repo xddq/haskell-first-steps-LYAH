445f7e064fb6be0d15258b75198160d1f6a55276
git@pierrefhapp.dev <git@pierrefhapp.dev>
2021-10-30 14:41:27 +0200

    2h of thinking and trying to solve RPN. Try some different approaches. Currently created stack data type and started recursively defining the calc function. created some helper functions

--- src/FunctionalProblemSolving.hs
+++ src/FunctionalProblemSolving.hs
@@ -7,26 +7,94 @@
 -- RPN 10 4 3 + 2 * - --> 3+4=7;2*7=14;14-10=4; 4
 import qualified Data.Char as Char
 
+testArray = "10 4 2 * -"
+
 --
 calcRPN :: (Num a) => String -> a
 calcRPN input = do
-    let inputArray = words input
-    3
+  let numbers = filter representsNumber $ words input
+  let operators = filter representsOperator $ words input
+  let order = map representsNumber $ words input
+  3
 
--- Returns True if all entries represent a number [0-9]. Else returns False.
--- E.g. "100" "39" --> True
+data Action = PushStack | PopStack deriving (Show, Eq)
+
+-- basic minimal operation. if number -> put on stack, else pop from stack.
+-- use tuple to return result and resulting list.
+-- calc :: (Num a) => ([a], [MyOperator], [Action]) -> ([a], [MyOperator], [Action])
+-- calc (xs, _, []) = (xs, _, [])
+-- calc (numbers, operators, actions) = do
+--     if head actions == PushStack then calc (xs
+
+makeStack :: a -> Stack a
+makeStack input = input :-: EmptyStack
+
+calc :: [a] -> Stack a -> a
+calc [] stack = result
+    where (result, restStack) = stackPop stack
+    -- TODO(pierre): continue here!
+
+infixr 5 :-:
+data Stack a = EmptyStack | a :-: (Stack a) deriving (Show, Eq, Read, Ord)
+testStack = 2 :-: 3 :-: EmptyStack
+
+infixr 5 :.:
+data List a = Empty | a :.: (List a) deriving (Show, Eq, Read, Ord)
+testList = 2 :.: 3 :.: 4 :.: Empty
+
+
+stackPush :: Stack a -> a -> Stack a
+stackPush xs input = input:-:xs
+
+stackPop :: Stack a -> (a,Stack a)
+stackPop EmptyStack = undefined
+stackPop (x:-:xs) = (x, xs)
+
+-- class Stackable a where
+--     pop :: [a] -> a
+--     push :: [a] -> a -> [a]
+-- instance Stackable Stack a where
+--     push xs input = input:xs
+--     pop (x:xs) = x
+
+-- calc :: (Num a) => String -> [a] -> a
+-- -- NOTE(pierre): head is unsafe, how else to solve it?
+-- calc [] stack = head stack
+-- calc input stack = do
+--     let firstValue = head $ words input
+--     case representsNumber of input
+--         True -> calc (tail input) (stack:
+
+-- calc (acc, numbers, operators, actions)
+-- if order = PushStack then
+
+-- MAYBE(pierre): where are functions for safeTail, safeHead etc..?
+safeTail :: [a] -> [a]
+safeTail [] = []
+safeTail (x : xs) = xs
+
+returnNumber :: (Num a, Read a) => String -> Maybe a
+returnNumber input = if representsNumber input then Just $ read input else Nothing
+
 representsNumber :: String -> Bool
-representsNumber string = all (== True) $ map Char.isNumber string
+representsNumber = all ((== True) . Char.isNumber)
 
-helperRPN :: (Num a, Num b, Fractional b) => String -> String -> Either a b
-helperRPN input stack
-    | representsNumber $ input = Left $ read input
-    | otherwise = Right $ returnOperator input
+data MyOperator = Addition | Multiplication | Division deriving (Show, Eq)
 
+representsOperator :: String -> Bool
+representsOperator input =
+  let supportedOperators = words "+ - / *"
+   in if (==) 1 $ length input then elem input supportedOperators else False
 
-returnOperator :: (Num a, Fractional a) => String -> a -> a -> a
+returnOperator :: String -> Maybe MyOperator
 returnOperator input
-    | input == "+" = (+)
-    | input == "*" = (*)
-    | input == "/" = (/)
+  | input == "+" = Just Addition
+  | input == "*" = Just Multiplication
+  | input == "/" = Just Division
+  | otherwise = Nothing
 
+-- helperRPN :: (Num a, Num b, Fractional b) => String -> String -> Either a b
+-- helperRPN input stack = do
+--     case representsNumber input of
+--         Just number -> number
+--         Nothing -> -1
